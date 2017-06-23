75f78c03cdc935be3cf162be6534fe5b8390a69e
钰 <1003656685@qq.com>
2017-06-23 12:19:04 +0800

    add index.js

---
+++ Js/index.js
@@ -0,0 +1,21 @@
+//首页事件
+function btn1(){
+	window.location.href="single_row_layout/single_row_layout.html";
+}
+
+
+
+
+
+//单列布局事件
+function btn2(){
+	window.location.href="single_row_layout1.html";
+}
+function btn3(){
+	window.location.href="banner.html";
+}
+function btn4(){
+	window.location.href="banner1.html";
+}
+
+//双列布局事件
